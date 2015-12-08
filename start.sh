6215e45f47a45ba046a4f18634d8dc9cd7606524
Alan Harnum <aharnum@ocadu.ca>
2015-12-08 09:45:27 -0500

    Runtime variables .yml file used with --extra-vars flag, to ensure it takes precedence over anything else when running Ansible playbook

--- start.sh
+++ start.sh
@@ -7,6 +7,14 @@ cat > run_vars.yml<<EOF
 preferences_server_couchdb_host_address: $COUCHDB_HOST_ADDRESS
 preferences_server_environment: $NODE_ENV
 preferences_server_prime_db: $PRIME_DB
+
+nodejs_app_host_address: 127.0.0.1
+nodejs_app_tcp_port: 8082
+nodejs_app_test_http_endpoint: /preferences/carla
+nodejs_app_test_http_status_code: 200
+nodejs_app_test_string: registry.gpii.net
+
 EOF
 
-ansible-playbook run.yml --tags "deploy" && supervisord -n -c /etc/supervisord.conf
+ansible-playbook run.yml --tags "deploy" --extra-vars "@run_vars.yml" && \
+supervisord -n -c /etc/supervisord.conf
